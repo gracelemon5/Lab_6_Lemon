08fa3119be72bdbdf3a699420792d15c9c1d0dc9
Grace Renee Lemon <lemon5@purdue.edu>
2023-03-30 11:39:35 -0400

    Add project files.

---
+++ Lab_6_Lemon/Lab_6_Lemon.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
+int main() {
+	string background = "images1/backgrounds/winter.png";
+	string foreground = "images1/characters/yoda.png";
+	Texture backgroundTex;
+	if (!backgroundTex.loadFromFile(background)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+	Texture foregroundTex;
+	if (!foregroundTex.loadFromFile(foreground)) {
+		cout << "Couldn't Load Image" << endl;
+		exit(1);
+	}
+	Image backgroundImage;
+	backgroundImage = backgroundTex.copyToImage();
+	Image foregroundImage;
+	foregroundImage = foregroundTex.copyToImage();
+	Vector2u sz = backgroundImage.getSize();
+	for (int y = 0; y < sz.y; y++)
+	{
+		for (int x = 0; x < sz.x; x++)
+		{
+			const Color pixel = foregroundImage.getPixel(x, y);
+			if (pixel.r == 32 && pixel.b == 23 && pixel.g == 214) //Greenscreen
+			{
+				foregroundImage.setPixel(x, y, backgroundImage.getPixel(x, y));
+			}
+		}
+	}
+
+	// By default, just show the foreground image
+	RenderWindow window(VideoMode(1024, 768), "Here's the output");
+	Sprite sprite1;
+	Texture tex1;
+	tex1.loadFromImage(foregroundImage);
+	sprite1.setTexture(tex1);
+	window.clear();
+	window.draw(sprite1);
+	window.display();
+	while (true);
+}
